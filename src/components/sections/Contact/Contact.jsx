57751bfde9713fca07b8c8e8b518f166b0b776e5
RolDev-22 <rolandomuagui1629@gmail.com>
2026-09-23 12:09:22 -0600

    Se agrega la imagen preview del proyecto, se crea el componente Form para modularizar la secciion de contacto y se genera el css modulo respectivo

--- src/components/sections/Contact/Contact.jsx
+++ src/components/sections/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import styles from "./Contact.module.css";
+import { FormContact } from "@ui/Form/FormContact";
 import { MdOutlineAlternateEmail } from "react-icons/md";
 import { FaPhone } from "react-icons/fa6";
 import { FaMapMarkedAlt } from "react-icons/fa";
@@ -20,44 +21,7 @@ export default function Contact() {
               Estoy aquí para responder tus dudas y escuchar tus ideas
             </p>
           </div>
-          <form
-            action=""
-            className=" p-2 m-0 d-flex flex-column gap-2 rounded-3 col-12">
-            <div className="apperEffectDown container d-flex flex-column">
-              <label htmlFor="inptName">Nombre</label>
-              <input
-                className="form-control shadow-none"
-                type="text"
-                name="inptName"
-                id="inptName"
-              />
-            </div>
-            <div className="container d-flex flex-column">
-              <label htmlFor="inptMail">Correo</label>
-              <input
-                className="form-control shadow-none"
-                type="email"
-                name="inptMail"
-                id="inptMail"
-              />
-            </div>
-            <div className="container d-flex flex-column">
-              <label htmlFor="inptMessage">Mensaje</label>
-              <textarea
-                className={`${styles.textAreaForm} form-control shadow-none`}
-                name="inptMessage"
-                id="inptMessage"></textarea>
-            </div>
-            <div className="container d-flex gap-2 sharedSelect">
-              <input type="checkbox" name="inptCheck" id="inptCheck" />
-              <label htmlFor="inptCheck">
-                Consentimiento acuerdo requerido
-              </label>
-            </div>
-            <div className="container d-flex justify-content-center">
-              <button className="btn btn-outline-info w-75">Enviar</button>
-            </div>
-          </form>
+          <FormContact />
         </div>
         <div
           className={`apperEffect ${styles.infoContactStyle} container-fluid d-flex flex-column align-items-center order-md-2 rounded-2 justify-content-center`}>
---
+++ src/components/ui/Form/FormContact.jsx
@@ -0,0 +1,42 @@
+import styles from "./FormContact.module.css";
+
+export const FormContact = () => {
+  return (
+    <form
+      action=""
+      className=" p-2 m-0 d-flex flex-column gap-2 rounded-3 col-12">
+      <div className="apperEffectDown container d-flex flex-column">
+        <label htmlFor="inptName">Nombre</label>
+        <input
+          className="form-control shadow-none"
+          type="text"
+          name="inptName"
+          id="inptName"
+        />
+      </div>
+      <div className="container d-flex flex-column">
+        <label htmlFor="inptMail">Correo</label>
+        <input
+          className="form-control shadow-none"
+          type="email"
+          name="inptMail"
+          id="inptMail"
+        />
+      </div>
+      <div className="container d-flex flex-column">
+        <label htmlFor="inptMessage">Mensaje</label>
+        <textarea
+          className={`${styles.textAreaForm} form-control shadow-none`}
+          name="inptMessage"
+          id="inptMessage"></textarea>
+      </div>
+      <div className="container d-flex gap-2 sharedSelect">
+        <input type="checkbox" name="inptCheck" id="inptCheck" />
+        <label htmlFor="inptCheck">Consentimiento acuerdo requerido</label>
+      </div>
+      <div className="container d-flex justify-content-center">
+        <button className="btn btn-outline-info w-75">Enviar</button>
+      </div>
+    </form>
+  );
+};
